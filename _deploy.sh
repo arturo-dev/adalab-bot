a5dcde11755e7f029bee2843aff3482238b967df
Arturo LR <arturolr@Arturos-MacBook-Pro.local>
2019-02-19 14:49:17 +0100

    misc changes

--- _deploy.sh
+++ _deploy.sh
@@ -1,2 +1,3 @@
+gcloud config set project adalab
 gcloud auth activate-service-account --key-file auth.json
 gcloud app deploy app.yml -q
--- index.js
+++ index.js
@@ -2,18 +2,24 @@ const TOKEN = 'NTQ2ODExNjg0NzYxODk0OTMz.D0trLA.CsC4JA9bkKxITCED_yHSdtI1gPg';
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const dispatcher = require('./dispatcher/dispatcher');
-const CHANNEL_GENERAL = '546693851243479053';
+const CHANNEL_BOT = '547413680892149761';
+
+const MESSAGES_BOT = [
+  `
+  Try the ADALAB bot can be helpfull (type !help for available commands)
+  Try the Google bot (type g!help for available commands)
+  If you wanna contribute tell it to an admin'
+  `,
+  'If you are bored do an exercise or learn more',
+  'You can watch and contribute me, my code is on https://github.com/arturo-dev/adalab-bot'
+];
 
 client.once('ready', () => {
   console.log(`Ready ${client.user.username}`);
   setInterval(() => {
-    client.channels.get(CHANNEL_GENERAL).send(
-      `
-      Try the ADALAB bot can be helpfull (type !help for available commands)
-      Try the Google bot (type g!help for available commands)
-      If you wanna contribute tell it to an admin'
-      `
-    );
+    client.channels
+      .get(CHANNEL_BOT)
+      .send(MESSAGES_BOT[Math.floor(Math.random() * MESSAGES_BOT.length) + 0]);
   }, 540000);
 });
 
