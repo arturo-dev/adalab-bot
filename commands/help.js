99866b78b728661444f27ace412c2a0459efa1fa
Arturo LR <arturolr@Arturos-MacBook-Pro.local>
2019-02-19 12:52:18 +0100

    misc changes

--- commands/help.js
+++ commands/help.js
@@ -6,6 +6,9 @@ module.exports = (message, command) => {
     info.push(`- !${commands[i].cmd}: ${commands[i].desc}`);
   }
 
+  message.channel.send(`
+    ${message.author.username} I've DM'd you a list of my commands.
+  `);
   message.author.send(
     `
     Description: ${command.desc}
